dcde39318353b963a9a71a9692371bc0f9a305f7
zfkdnop <zfkdnop@gmail.com>
2021-10-28 02:03:24 -0500

    updated Modal.js and added a few comments

--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -1,30 +1,51 @@
-/*
- * NOTE: We could use refs to get a line on the buttons or other elements...
- *   const modalNegBtn = useRef();
- *   const modalAffBtn = useRef();
- *   <Modal modalID='errormodal' negRef={modalNegBtn} affRef={modalAffBtn}
- *     ... (in ModalFooter) ... <button ref={props.negRef}> ... <button ref={props.affRef}> ....
+/* 
+ * 
  */
 import { createPortal } from 'react-dom';
 
+/* Modal Component (primary component)
+ * * * * * * * * * * * * * * * * */
+export function Modal({ modalID, children }) {
+  if (
+    children === undefined ||
+    modalID === undefined ||
+    String(modalID).trim().length <= 0
+  )
+    return null;
+  return createPortal(
+    <ModalDialog modalID={String(modalID).trim()}>{children}</ModalDialog>,
+    document.getElementById('overlay-root')
+  );
+}
+
+/* other Components (secondary components)
+ * * * * * * * * * * * * * * * * */
 export function ModalFooter({
-  negCallback,
-  negTitle = 'Cancel',
-  affCallback,
-  affTitle = 'Okay',
-  children,
+  affCallback,          /* called when the affirmative butotn is clicked (optional) */
+  affTitle = 'Okay',    /* caption of the affirmative button */
+  affStyle = 'primary', /* used to finish the `btn-_____' bootstrap class and add additional classes to the affirmative button */
+  negCallback,          /* callback called when negative button clicked */
+  negTitle,             /* caption of the negative button. if undefined, the negative button will not be rendered */
+  negStyle = 'secondary', /* use this to finish the `btn-_____' bootstrap class and add additional classes to the negative button */
+  children,             /* react */
 }) {
-  return (
-    <div className='modal-footer'>
-      {children}
+  let negBtn = <></>;
+  if (String(negTitle).trim() !== '' && negTitle !== undefined)
+    negBtn = (
       <button
-        className='btn btn-secondary'
+        className={'btn btn-' + negStyle}
         onClick={negCallback}
         data-bs-dismiss='modal'>
         {negTitle}
       </button>
+    );
+
+  return (
+    <div className='modal-footer'>
+      {children}
+      {negBtn}
       <button
-        className='btn btn-primary'
+        className={'btn btn-' + affStyle}
         onClick={affCallback}
         data-bs-dismiss='modal'>
         {affTitle}
@@ -37,6 +58,9 @@ export function ModalBody({ children }) {
   return <div className='modal-body'>{children}</div>;
 }
 
+export function ModalHeader({ children }) {
+  return <ModalTitle>{children}</ModalTitle>;
+}
 export function ModalTitle({ children }) {
   return (
     <div className='modal-header'>
@@ -53,13 +77,5 @@ function ModalDialog({ modalID, children }) {
         <div className='modal-content'>{children}</div>
       </div>
     </div>
-  );
-}
-
-export default function Modal({ modalID, children }) {
-  if (children === undefined || modalID === undefined || String(modalID).trim().length <= 0) return null;
-  return createPortal(
-    <ModalDialog modalID={String(modalID).trim()}>{children}</ModalDialog>,
-    document.getElementById('overlay-root')
   );
 }
