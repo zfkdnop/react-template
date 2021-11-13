9757b3a01a67925f60fc2a6a35fb2dab42693cdf
zfkdnop <zfkdnop@gmail.com>
2021-11-12 23:34:51 -0600

    add programmatic modal display via useRef/forwardRef in Modal.js

--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -1,41 +1,59 @@
-/* 
- * 
+/*
+ *
  */
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 /* Modal Component (primary component)
  * * * * * * * * * * * * * * * * */
-export function Modal({ modalID, children }) {
-  if (
-    children === undefined ||
-    modalID === undefined ||
-    String(modalID).trim().length <= 0
-  )
-    return null;
+export const Modal = forwardRef(({ modalID, children }, ref) => {
+  const refShowModalBtn = useRef();
+  const handleShow = () => {
+    refShowModalBtn.current.click();
+  };
+  useImperativeHandle(ref, () => {
+    return { show: handleShow };
+  });
+
+  if (children === undefined) return null; /* if no children, dont render */
+  if (modalID === undefined || String(modalID).trim().length <= 0) /* if no modalID specified, generate one for internal use */
+    modalID = 'modal' + Math.round(Math.random() * 100_000_000);
+
   return createPortal(
-    <ModalDialog modalID={String(modalID).trim()}>{children}</ModalDialog>,
+    <>
+      <button
+        className='visually-hidden d-none'
+        ref={refShowModalBtn}
+        type='button'
+        data-bs-toggle='modal'
+        data-bs-target={'#'+modalID}>
+        _
+      </button>
+      <div className='modal fade' tabIndex='-1' id={modalID}>
+        <div className='modal-dialog modal-dialog-centered'>
+          <div className='modal-content'>{children}</div>
+        </div>
+      </div>
+    </>,
     document.getElementById('overlay-root')
   );
-}
+});
 
 /* other Components (secondary components)
  * * * * * * * * * * * * * * * * */
 export function ModalFooter({
-  affCallback,          /* called when the affirmative butotn is clicked (optional) */
-  affTitle = 'Okay',    /* caption of the affirmative button */
-  affStyle = 'primary', /* used to finish the `btn-_____' bootstrap class and add additional classes to the affirmative button */
-  negCallback,          /* callback called when negative button clicked */
-  negTitle,             /* caption of the negative button. if undefined, the negative button will not be rendered */
-  negStyle = 'secondary', /* use this to finish the `btn-_____' bootstrap class and add additional classes to the negative button */
-  children,             /* react */
+  affCallback /* called when the affirmative butotn is clicked (optional) */,
+  affTitle = 'Okay' /* caption of the affirmative button */,
+  affStyle = 'primary' /* used to finish the `btn-_____' bootstrap class and add additional classes to the affirmative button */,
+  negCallback /* callback called when negative button clicked */,
+  negTitle /* caption of the negative button. if undefined, the negative button will not be rendered */,
+  negStyle = 'secondary' /* use this to finish the `btn-_____' bootstrap class and add additional classes to the negative button */,
+  children /* react */,
 }) {
   let negBtn = <></>;
   if (String(negTitle).trim() !== '' && negTitle !== undefined)
     negBtn = (
-      <button
-        className={'btn btn-' + negStyle}
-        onClick={negCallback}
-        data-bs-dismiss='modal'>
+      <button className={'btn btn-' + negStyle} onClick={negCallback} data-bs-dismiss='modal'>
         {negTitle}
       </button>
     );
@@ -44,10 +62,7 @@ export function ModalFooter({
     <div className='modal-footer'>
       {children}
       {negBtn}
-      <button
-        className={'btn btn-' + affStyle}
-        onClick={affCallback}
-        data-bs-dismiss='modal'>
+      <button className={'btn btn-' + affStyle} onClick={affCallback} data-bs-dismiss='modal'>
         {affTitle}
       </button>
     </div>
@@ -66,16 +81,6 @@ export function ModalTitle({ children }) {
     <div className='modal-header'>
       <span className='modal-title'>{children}</span>
       <button className='btn-close' data-bs-dismiss='modal'></button>
-    </div>
-  );
-}
-
-function ModalDialog({ modalID, children }) {
-  return (
-    <div className='modal fade' tabIndex='-1' id={modalID}>
-      <div className='modal-dialog modal-dialog-centered'>
-        <div className='modal-content'>{children}</div>
-      </div>
     </div>
   );
 }
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,18 +2,33 @@
  *
  *
  */
-import Modal, { ModalTitle, ModalBody, ModalFooter } from '../layout/Modal';
+import { useRef } from 'react';
+import { Modal, ModalTitle, ModalBody, ModalFooter } from '../layout/Modal';
 
 export default function HomePage() {
+  const refModal = useRef();
+  const showModal = () => {
+    refModal.current.show();
+  };
+
   return (
     <>
-      <div className='card card-body text-center'>home
-        <button className='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modTest'>Modal</button>
+      <div className='card card-body text-center'>
+        home
+        <button className='btn btn-primary' onClick={showModal}>
+          Modal via ref v1
+        </button>
+        <button className='btn btn-primary' onClick={() => {refModal.current.show()}}>
+          Modal via ref v2
+        </button>
+        <button className='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modTest'>
+          Modal via standard bootstrap HTML/CSS-API
+        </button>
       </div>
-      <Modal modalID='modTest'>
-          <ModalTitle>Test: Modal</ModalTitle>
-          <ModalBody>Modal body</ModalBody>
-          <ModalFooter negTitle='Nah' affTitle='Yep' />
+      <Modal /* modalID='modTest' */ ref={refModal}>
+        <ModalTitle>Test: Modal</ModalTitle>
+        <ModalBody>Modal body</ModalBody>
+        <ModalFooter negTitle='Nah' affTitle='Yep' />
       </Modal>
     </>
   );
